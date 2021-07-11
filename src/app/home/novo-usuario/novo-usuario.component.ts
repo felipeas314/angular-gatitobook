cb2d58d7dc2618004cf667108f70274fd730b082
Felipe <felipeas314@gmail.com>
2021-07-11 17:16:14 -0300

    new user service creation

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor() { }
+  novoUsuarioForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      fullName: ['',[
+        Validators.required,
+        Validators.minLength
+      ]],
+      userName: [''],
+      password: ['']
+    })
+  }
+
+  cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log('novoUsuario:',novoUsuario);
   }
 
 }
