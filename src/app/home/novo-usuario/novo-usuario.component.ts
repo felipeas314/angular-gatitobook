46006a8ee7edec51862899371753db02d9c6c67b
Felipe <felipeas314@gmail.com>
2021-07-14 22:41:43 -0300

    validaçoes customizadas

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { minusculoValidator } from './minusculo.validator';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 
@@ -27,7 +28,10 @@ export class NovoUsuarioComponent implements OnInit {
       ]],
       userName: ['', [minusculoValidator]],
       password: ['']
-    })
+    },
+      {
+        validators: [usuarioSenhaIguaisValidator],
+      })
   }
 
   cadastrar() {
